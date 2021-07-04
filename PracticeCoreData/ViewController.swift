a5ecb9f4a3edf66641462b391ce783e28f6292df
gustn3965 <gustn3965@gmail.com>
2021-07-04 21:29:52 +0900

    Initial Commit

---
+++ PracticeCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeCoreData
+//
+//  Created by hyunsu on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
